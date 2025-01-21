1e073cd8d22a14384a56b4039121ee79c3700b0c
{AngelCortesM} <joseangelcortesmontoya@gmail.com>
2025-01-21 18:39:10 -0500

    Actualizar configuración de la aplicación para usar provideHttpClient con interceptores y limpiar importaciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
         },
       })
     ),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
   ],
 };
