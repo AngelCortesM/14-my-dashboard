62b3c5e513b052ba8eaafa0516379c0dc6e2fc1b
{AngelCortesM} <joseangelcortesmontoya@gmail.com>
2025-01-22 10:04:42 -0500

    Agregar lógica de productos dinámicos y mejorar el componente de tarjeta de producto

--- src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
+++ src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, effect, input, output } from '@angular/core';
+import { Product } from '@interfaces/product.interface';
 
 @Component({
   selector: 'app-product-card',
   imports: [],
   templateUrl: './product-card.component.html',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  public product = input.required<Product>();
+  public onIncrementQuantity = output<number>();
+  public incrementQuantity(): void {
+    this.onIncrementQuantity.emit(this.product().quantity + 1);
+  }
+
+  public loginEffect = effect(() => {
+    console.log(this.product().name);
+  });
+}
